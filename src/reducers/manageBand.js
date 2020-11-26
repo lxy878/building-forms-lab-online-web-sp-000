768449d15e132b137c078ac0c1078362e08f54c8
Xiaoyi Liu <lxy878@gmail.com>
2020-11-26 15:58:29 +0000

    Automatically backed up by Learn

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -2,10 +2,10 @@ export default function manageBand(state = {
   bands: [],
 }, action) {
   switch(action.type){
-  case('ADD_BAND'):
-    return {...state, bands: [...state.bands, action.band]}
+    case('ADD_BAND'):
+      return {...state, bands: [...state.bands, action.band]}
 
-  default:
-    return state
-}
+    default:
+      return state
+  }
 };
